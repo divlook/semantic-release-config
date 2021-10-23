ee24270db5c039ee17d84d1c15ebfb97fb86adb5
uihyeon.cha <uihyeon.cha@gmail.com>
2021-10-23 20:39:48 +0900

    init

---
+++ .releaserc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'angular',
+                releaseRules: [
+                    {
+                        type: 'docs',
+                        release: 'patch',
+                    },
+                    {
+                        type: 'refactor',
+                        release: 'patch',
+                    },
+                ],
+            },
+        ],
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        // '@semantic-release/github',
+        [
+            '@semantic-release/npm',
+            {
+                npmPublish: false,
+            },
+        ],
+        '@semantic-release/git',
+    ],
+    branches: 'main',
+}
