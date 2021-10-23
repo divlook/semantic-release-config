e21e5c08bafbb85083a685fc37b9a852f82fd9a4
uihyeon.cha <uihyeon.cha@gmail.com>
2021-10-23 21:47:08 +0900

    chore: 설정 수정

--- .releaserc.js
+++ .releaserc.js
@@ -1,29 +1,44 @@
+const releaseRules = [
+    {
+        type: 'build',
+        release: 'patch',
+    },
+    {
+        type: 'ci',
+        release: 'patch',
+    },
+    {
+        type: 'chore',
+        release: 'patch',
+    },
+    {
+        type: 'docs',
+        release: 'patch',
+    },
+    {
+        type: 'refactor',
+        release: 'patch',
+    },
+    {
+        type: 'style',
+        release: 'patch',
+    },
+    {
+        type: 'test',
+        release: 'patch',
+    },
+]
+
+const pluginOption = {
+    preset: 'angular',
+    releaseRules,
+}
+
 module.exports = {
     plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                preset: 'angular',
-                releaseRules: [
-                    {
-                        type: 'docs',
-                        release: 'patch',
-                    },
-                    {
-                        type: 'refactor',
-                        release: 'patch',
-                    },
-                ],
-            },
-        ],
-        '@semantic-release/release-notes-generator',
-        [
-            '@semantic-release/changelog',
-            {
-                preset: 'angular',
-            },
-        ],
-        // '@semantic-release/github',
+        ['@semantic-release/commit-analyzer', pluginOption],
+        ['@semantic-release/release-notes-generator', pluginOption],
+        ['@semantic-release/changelog', pluginOption],
         [
             '@semantic-release/npm',
             {
@@ -31,6 +46,7 @@ module.exports = {
             },
         ],
         '@semantic-release/git',
+        // '@semantic-release/github',
     ],
     branches: 'main',
     ci: false,
